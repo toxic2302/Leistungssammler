2960a70f04116fdf98c16d9564cf31a320b07796
Florian Kolb <florian.kolb@mni.thm.de>
2014-12-04 20:57:40 +0100

    24 stunden option entfernt

--- www/js/config.js
+++ www/js/config.js
@@ -3,5 +3,5 @@
  */
 angular.module('firebase')
     .constant('FIREBASE_URL', 'https://scorching-heat-4484.firebaseio.com/')
-    .constant('MODULE_UPDATE_INTERVALL', 86400000)
+    //.constant('MODULE_UPDATE_INTERVALL', 86400000)
 ;
